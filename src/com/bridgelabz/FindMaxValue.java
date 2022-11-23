208975c21178181130d8cf785da403163ee0dbe7
Your Name <you@example.com>
2022-11-23 15:21:28 +0530

    Modified: using generic class

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -1,33 +1,37 @@
 package com.bridgelabz;
 
-public class FindMaxValue {
+public class FindMaxValue<T extends Comparable<T>> {
+
+    T a,b,c;
+
+    public FindMaxValue(T a,T b,T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
     /* create maxOfString method and passing argumants */
 
-    public static <T extends Comparable> void maxOfValue(T a,T b,T c ) {
+    public static <T extends Comparable<T>> T maxOfValue(T a,T b,T c ) {
 
         T max = a;
-        if (b.compareTo(a) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
 
+        if (b.compareTo(a) > 0)
+
+            max = b;
+
+        if (c.compareTo(max) > 0)
+            max = c;
+            return max;
         }
-        System.out.println("Maximum String is: " + max);
-    }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome To Find Maximum value program using generics \n");
+        System.out.println("The maximum value between three Integer is: " + maxOfValue(12,11,13));
+        System.out.println("The maximum value between three Float is: " + maxOfValue(1.2f,1.1f,1.3f));
+        System.out.println("The maximum value between three String is: " + maxOfValue("Apple","Peach","Banana"));
 
-        Integer a=12,b=11,c=13;
-        Float p=1.2f,q=1.1f,r=1.3f;
-        String s="Apple",t="Peach",u="Banana";
-
-        FindMaxValue.maxOfValue(a,b,c);
-        FindMaxValue.maxOfValue(p,q,r);
-        FindMaxValue.maxOfValue(s,t,u);
     }
 }
 
